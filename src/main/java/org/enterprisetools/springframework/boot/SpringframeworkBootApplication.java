aa3983439e2a70ad9a80ef7bf5b1e0a917b1f8eb
Gurubrahmanandam E <guru.prince8@gmail.com>
2025-01-01 14:03:20 -0600

    spring boot initial set up

---
+++ src/main/java/org/enterprisetools/springframework/boot/SpringframeworkBootApplication.java
@@ -0,0 +1,13 @@
+package org.enterprisetools.springframework.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringframeworkBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/enterprisetools/springframework/boot/SpringframeworkBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.enterprisetools.springframework.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
